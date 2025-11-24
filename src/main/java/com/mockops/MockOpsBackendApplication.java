10932136d16a1da4776910a6dad697ce4fd6ff8e
kwang2134 <kwang2134@naver.com>
2025-11-24 16:20:23 +0900

    Init

---
+++ src/main/java/com/mockops/MockOpsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mockops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockOpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockOpsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mockops/mockops/MockOpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockops.mockops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockOpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
