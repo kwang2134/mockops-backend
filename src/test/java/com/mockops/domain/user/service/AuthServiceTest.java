7f49e05c483fdc6bf79e5b79ee1eb8b2ac5df586
kwang2134 <kwang2134@naver.com>
2025-11-28 23:31:55 +0900

    refactor(test-auth): test 업데이트

--- src/test/java/com/mockops/domain/user/service/AuthServiceTest.java
+++ src/test/java/com/mockops/domain/user/service/AuthServiceTest.java
@@ -9,7 +9,7 @@ import com.mockops.domain.user.role.Role;
 import com.mockops.global.exception.BusinessException;
 import com.mockops.global.security.JwtProvider;
 import com.mockops.global.util.CryptUtils;
-import com.mockops.presentaion.api.user.dto.TokenResponse;
+import com.mockops.presentation.api.user.dto.TokenResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
